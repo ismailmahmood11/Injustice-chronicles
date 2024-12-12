2712bc424be550c8e05e569b0cbf63035187275b
ismailmahmood11 <97909583+ismailmahmood11@users.noreply.github.com>
2024-12-13 02:33:48 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,20 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "cbcaa8833d87123fcad399b50d35cef4",
+const RESOURCES = {"flutter_bootstrap.js": "8cca75e12f76c7a6a024ccedfcafe33a",
 "version.json": "98ca0418587216cfd97a1bdf31a30f1a",
-"index.html": "8b2905cf4b7259c1bea6f4dd1ad18fbf",
-"/": "8b2905cf4b7259c1bea6f4dd1ad18fbf",
+"index.html": "a39fede11561f43476b8dc2aa3ea278f",
+"/": "a39fede11561f43476b8dc2aa3ea278f",
 "main.dart.js": "15b8b551174a0ef69410aeea827320fe",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.mjs": "7caab9a5e403c1875c897c72f4ef1357",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "47ed0778f003e1d87c0ff46c12e5111f",
-"main.dart.wasm": "f70ccb61c60c2d3fc82a8843a1b3ce00",
 "assets/AssetManifest.json": "84c6a3b98d3fd89ac34bf19284cb9690",
 "assets/NOTICES": "52c511da9462dce6dc49e22d6b9d9886",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
@@ -47,8 +45,6 @@ const RESOURCES = {"flutter_bootstrap.js": "cbcaa8833d87123fcad399b50d35cef4",
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
-"main.dart.wasm",
-"main.dart.mjs",
 "index.html",
 "flutter_bootstrap.js",
 "assets/AssetManifest.bin.json",
